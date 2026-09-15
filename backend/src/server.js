c08dcba700503e7be1c3785a70702d67057fed3b
codenexusjitb <codenexus.jitb@gmail.com>
2026-09-16 04:16:56 +0530

    Fix Express 5 wildcard routing error

--- backend/src/server.js
+++ backend/src/server.js
@@ -37,7 +37,7 @@ app.get('/health', (req, res) => {
 app.use(express.static(path.join(__dirname, '../../website/dist')));
 
 // Fallback for React Router
-app.get('*', (req, res) => {
+app.get(/.*/, (req, res) => {
     res.sendFile(path.join(__dirname, '../../website/dist/index.html'));
 });
 
